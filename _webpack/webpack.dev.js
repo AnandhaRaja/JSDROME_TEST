0407b7322ee1f3b9a4d7daf8360091b0a90bf2db
Anandha Raja S S S <AnandhaRajaSSS@users.noreply.github.com>
2020-02-19 00:34:53 +0530

    TEST COUNTER

--- _webpack/webpack.dev.js
+++ _webpack/webpack.dev.js
@@ -1,6 +1,15 @@
 const merge = require('webpack-merge');
+const path = require('path');
+const webpack = require('webpack');
 const commonConfig = require('./webpack.common');
 
 module.exports = merge(commonConfig, {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '../_dist'),
+    compress: true,
+    hot: true,
+    port: 9000,
+  },
 });
--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
-class App extends Component {
+if(module.hot) module.hot.accept();
+
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  multiply = () => {
+    let currentCount = this.state.count;
+
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: currentCount * 2,
+    });
+  };
+  divider = () => {
+    let currentCount = this.state.count;
+
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count: currentCount / 2,
+    });
+  };
+  resetss = () => {
+
+    this.setState({
+      // eslint-disable-next-line no-magic-numbers
+      count:  0,
+    });
+  };
+
   render() {
-    return <h1>JSDROM, TEST</h1>;
+    return <div>
+      <h1>{this.state.count}</h1>
+      <button onClick={this.multiply}>*</button>
+      <button onClick={this.divider}>/</button>
+      <button onClick={this.resetss}>Reset</button>
+    </div>
   }
 }
 
